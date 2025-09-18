84fc363cd541e62e5930c5d6226191389e3cc71c
Jenifer29b <Jenifer29b@gmail.com>
2025-09-18 18:56:02 +0530

    switch case

--- js/script.js
+++ js/script.js
@@ -4,6 +4,26 @@ console.log(a + b)
 
 // alert("hello")
 
-let x = prompt("enter an number")
-console.log(x)
-console.log((Number(x) + 10))
+// let x = prompt("enter an number")
+// console.log(x)
+// console.log((Number(x) + 10))
+
+let mark = Number(prompt("enter your mark"));
+
+switch (true) {
+    case (mark >= 90):
+        console.log("Grade A");
+        break;
+    case (mark >= 80):
+        console.log("Grade B");
+        break;
+    case (mark >= 70):
+        console.log("Grade C");
+        break;
+    case (mark >= 60):
+        console.log("Grade D");
+        break;
+    default:
+        console.log("Grade E");
+        break;
+}
