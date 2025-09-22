c5b510301fda184d653c9a09bca6f2515d3814e3
Jenifer29b <Jenifer29b@gmail.com>
2025-09-22 17:03:06 +0530

    example tasks

---
+++ Day 5/task1.js
@@ -0,0 +1,22 @@
+// for (let i = 1; i <= 10; i++){
+//     console.log("2", "*" , i ,"=" , (i*2) )
+// }
+
+// let num = 2;
+// for (let i = 1; i <= 10; i++){
+//     console.log(`${num} * ${i} = ${i*2}` )
+// }
+
+let n = 2
+for (let i = 1; i <= 5; i++){
+    console.log(n += i) // n = n + i
+}
+
+// let a = 5
+let fact= 1
+for (let i = 1; i <= 5; i++){
+  (fact*=i)
+}
+console.log("fact" ,fact)
+
+
