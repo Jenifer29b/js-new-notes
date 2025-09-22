2e25a78fdcf68f5fad085d4f8192247823a28045
Jenifer29b <Jenifer29b@gmail.com>
2025-09-22 21:10:51 +0530

    dowhile tasks

--- Day 5/task3.js
+++ Day 5/task3.js
@@ -6,13 +6,30 @@
 //     i++;
 // }
 
-let a = 1;
-while (a<=30) {
-    if (a % 2 !== 0) {
-        console.log(a)
-    }
-    a++
-}
+// let a = 1;
+// while (a<=30) {
+//     if (a % 2 !== 0) {
+//         console.log(a)
+//     }
+//     a++
+// }
+
+//do while loop
+
+
+// let num = 1
+
+// do {
+//     console.log(num)
+//     num++
+// } while (num <= 5);
+
+let sq = 1;
+
+do {
+    console.log(sq*=sq)
+    sq++
+}while(sq<=10)
 
 
 
--- js/script.js
+++ js/script.js
@@ -8,22 +8,40 @@ console.log(a + b)
 // console.log(x)
 // console.log((Number(x) + 10))
 
-let mark = Number(prompt("enter your mark"));
+// let mark = Number(prompt("enter your mark"));
 
-switch (true) {
-    case (mark >= 90):
-        console.log("Grade A");
-        break;
-    case (mark >= 80):
-        console.log("Grade B");
-        break;
-    case (mark >= 70):
-        console.log("Grade C");
-        break;
-    case (mark >= 60):
-        console.log("Grade D");
-        break;
-    default:
-        console.log("Grade E");
-        break;
-}
+// switch (true) {
+//     case (mark >= 90):
+//         console.log("Grade A");
+//         break;
+//     case (mark >= 80):
+//         console.log("Grade B");
+//         break;
+//     case (mark >= 70):
+//         console.log("Grade C");
+//         break;
+//     case (mark >= 60):
+//         console.log("Grade D");
+//         break;
+//     default:
+//         console.log("Grade E");
+//         break;
+// }
+
+// let num = Number(prompt("enter the number : "))
+// console.log(num)
+
+// while (num==0) {
+//     console.log("zero")
+//     num++
+// }
+// console.log(num)
+
+let pass = prompt("enter the password")
+
+do {
+    
+    console.log("password ok")
+} while (pass === "admin123");
+
+console.log("password not ok")
