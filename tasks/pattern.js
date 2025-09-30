185d466d9e92544c8db4acfaf14f5db6ecf1f0ec
Jenifer29b <Jenifer29b@gmail.com>
2025-09-30 18:01:32 +0530

    pattern tasks completed

--- tasks/pattern.js
+++ tasks/pattern.js
@@ -1,1 +1,12 @@
- 
+for (let i = 1; i <= 5; i++){
+    row = ''
+    for (let j = 1; j <= i; j++){
+        row += '*';
+    }
+    console.log(row)
+}
+
+for (let i = 1; i <= 5; i++){
+    console.log('*'.repeat(i))
+}
+
