08874705edb36b392007fa080787aac68a87bd5a
Jenifer29b <Jenifer29b@gmail.com>
2025-10-07 16:10:06 +0530

    day6 array  basics topics

---
+++ Day 6/array basics.js
@@ -0,0 +1,19 @@
+// An array is used to store an multipe values in single variable 
+// we can access an array , modify an array by using an indexces
+
+
+let arr = ["apple", "mango", "banana"]
+console.log(arr.push("kiwi"))//adding the element in end of array
+console.log(arr)
+console.log(arr.pop())  // removing the element in end of array
+console.log(arr.unshift("custard"))// adding the element in start of array
+console.log(arr)
+console.log(arr.shift()) // removing the element in starting of an array
+console.log(arr)
+
+let arr1 = ["birds", "names", "things", "animals", "toys"]
+console.log(arr1.length)
+console.log(arr1.indexOf("things"));
+console.log(arr1.includes("name"))
+arr1[2] = "place"
+console.log(arr1)
---
+++ Day 6/for types loop.js
@@ -0,0 +1,10 @@
+// there are two types of for loops variations like : for in , for of
+// for.in is used for access the indexes not the value directly in array
+// for.of is used for access the values directly in array,string , map etc.,
+
+
+let arr = ["apple", "mango", "banana"]
+
+for (let x in arr) {
+    console.log(x)
+}
