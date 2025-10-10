6a1fcb50ce48f399b9aab6658b97bcb7f2fdc04e
Jenifer29b <Jenifer29b@gmail.com>
2025-10-10 17:26:35 +0530

    while  loop task

--- Day 5/loops.js
+++ Day 5/loops.js
@@ -6,9 +6,9 @@ for (let i = 1; i <= 5; i++){
 }
 
 
-for (let i = 1; i <= 10; i++){
-    console.log(i)
-}
+// for (let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
 
 //while loop  :  it checks the condition and exceutes until the condition becomes 
 // true once the condiition becomes true it stops the exceution...
@@ -18,9 +18,15 @@ while (i <= 5) {
     console.log("while count :", i)
     i++
 }
+let x = 5;
+while(x>0){
+    console.log(x)
+    x--
+}
+
 
 // do while : in do while it checks the conditon only after the first exceution.. 
-// if condition is false it already finsih the exceution
+// if condition is false it  finsih the exceution
 
 let a = 2;
 do {
@@ -28,3 +34,6 @@ do {
     a++
     
 } while (a <= 5);
+
+
+
