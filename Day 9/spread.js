57279ba4dbb73308deadace6602113f2a035e6b2
Jenifer29b <Jenifer29b@gmail.com>
2025-10-28 22:46:40 +0530

    chnages in  files

--- Day 9/spread.js
+++ Day 9/spread.js
@@ -8,4 +8,21 @@ let combined = [...arr1, ...arr2]
 console.log(combined)
 
 let num1 = [1, 3, 2, 4, 5]
-console.log(6,7,...num1)
+console.log(6, 7, ...num1)
+
+// spread operator is also used for copying from original (shallow copy)
+let copy = [...arr1];
+copy.push(40)
+console.log(copy)
+
+let person = {
+    name: "Jeni",
+    place : "chennai"
+}
+let Job = {
+    work: "Full Stack Developer",
+    In : "Madurai"
+}
+
+let details = { ...person, ...Job }
+console.log(details)
